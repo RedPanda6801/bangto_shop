e5cc8fc944b34356a4e8de5ad00859badadf119e
sunny <parksunsunny@gmail.com>
2025-02-03 10:11:15 +0900

    2502053_로그인 페이지 수정

--- react/bangto_front/src/Components/UserLoginComponent.js
+++ react/bangto_front/src/Components/UserLoginComponent.js
@@ -7,31 +7,38 @@ const UserMainComponent = () =>
     <div className="layout_Content">
         <div className="layout_Top"/>
         <div className="box_login">
-            <table>
+            <table className="table_login">
               <tr>
                 <td>
                   <input
+                    className="input_email"
                     type="email"
                     placeholder="이메일"/>
                 </td>
               </tr>
             </table>
-            <table>
+            <table className="table_login">
               <tr>
                 <td>
                   <input
+                    className="input_pw"
                     type="password"
                     placeholder="비밀번호"/>
                 </td>
               </tr>
             </table>
             <input 
+              className="btn_login"
               type="button" 
               value="로그인"/>
-            <input 
-              type="button" 
-              value="소셜로그인"/>
+            <a href="https://kauth.kakao.com/oauth/authorize?client_id=0fd256147c0008703a4c880f0fb3b6a4&redirect_uri=http://localhost:3000&response_type=code">
+            <img
+              className="btn_social"
+              src={`${process.env.PUBLIC_URL}/images/02_icon/kakao_login_medium_narrow.png`} 
+              alt="카카오 로그인" />
+          </a>
           <div
+            className="btn_join"
             onClick={() => (window.location.href = '/join')}>
             회원가입
           </div>
