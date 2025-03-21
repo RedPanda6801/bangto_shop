29856337156bc0cb2830f311279f05805143d828
ParkSeunggyu <redpanda6801@gmail.com>
2025-03-21 09:13:02 +0900

    커밋

--- banto/src/main/java/com/example/banto/Repositorys/GroupBuyPayRepository.java
+++ banto/src/main/java/com/example/banto/Repositorys/GroupBuyPayRepository.java
@@ -19,8 +19,8 @@ public interface GroupBuyPayRepository extends JpaRepository<GroupItemPays, Inte
 	@Query("SELECT p FROM GroupItemPays p WHERE p.user.id = :userId")
 	public List<GroupItemPays> findByUserId(@Param("userId") Integer userId);
 
-	@Query(value = "SELECT * FROM group_item_pays p WHERE p.buyer_pk = :userId AND EXTRACT(YEAR FROM p.sold_date) = :year",
-			countQuery = "SELECT count(*) FROM group_item_pays p WHERE p.buyer_pk = :userId AND EXTRACT(YEAR FROM p.sold_date) = :year",
+	@Query(value = "SELECT * FROM group_item_pays p WHERE p.buyer_pk = :userId AND EXTRACT(YEAR FROM p.sold_date) = :year ORDER BY p.sold_Date DESC",
+			countQuery = "SELECT count(*) FROM group_item_pays p WHERE p.buyer_pk = :userId AND EXTRACT(YEAR FROM p.sold_date) = :year ORDER BY p.sold_date DESC",
 			nativeQuery = true)
 	public List<GroupItemPays> findByUserIdAndYear(@Param("userId") Integer userId, @Param("year") Integer year);
 
